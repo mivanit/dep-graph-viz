7c9c28bbc3ae98c35b1416da2c66b6e74af53304
mivanit <mivanits@umich.edu>
2023-07-19 17:16:10 -0400

    improved cli, added config options

--- dep_graph_viz/dep_graph_viz.py
+++ dep_graph_viz/dep_graph_viz.py
@@ -1,11 +1,57 @@
 import ast
 import glob
 import os
+import json
 from pathlib import Path
 from typing import Literal, Any
 
+from muutils.dictmagic import update_with_nested_dict, kwargs_to_nested_dict
 import networkx as nx
+import pydot
 from networkx.drawing.nx_pydot import to_pydot
+
+
+CONFIG: dict[str, Any] = {
+    "edge": {
+        "hierarchy": {
+            "color": "black",
+            "penwidth": "3",
+            "style": "solid",
+        },
+        "uses": {
+            "color": "red",
+            "penwidth": "2",
+            "style": "solid",
+        },
+        "inits": {
+            "color": "blue",
+            "penwidth": "1",
+            "style": "dashed",
+        },
+    },
+    "node": {
+        "dir": {
+            "shape": "folder",
+            "color": "black",
+        },
+        "file": {
+            "shape": "note",
+            "color": "black",
+        },
+    },
+    "dot_attrs": {
+        'rankdir': 'TB',
+    },
+}
+
+def _process_config() -> None:
+    global CONFIG
+    for key, value in CONFIG["edge"].items():
+        if isinstance(value, str) and value.lower() in ["none", "null"]:
+            CONFIG["edge"][key] = None
+    for key, value in CONFIG["node"].items():
+        if isinstance(value, str) and value.lower() in ["none", "null"]:
+            CONFIG["node"][key] = None
 
 
 def get_imports(source_code: str) -> list[str]:
@@ -47,7 +93,10 @@ def build_graph(python_files: list[str], root: str) -> nx.DiGraph:
     for python_file, module_name in zip(python_files, module_names):
 
         # Add node for module, with rank based on depth in hierarchy
-        G.add_node(module_name, rank=module_name.count("."), shape='box')
+        if "__init__" in python_file:
+            G.add_node(module_name, rank=module_name.count("."), **CONFIG["node"]["dir"])
+        else:
+            G.add_node(module_name, rank=module_name.count("."), **CONFIG["node"]["file"])
         
         # Read source code
         with open(python_file, "r", encoding="utf-8") as f:
@@ -60,7 +109,8 @@ def build_graph(python_files: list[str], root: str) -> nx.DiGraph:
         ]
         if module_parents:
             module_parent: str = sorted(module_parents, key=len)[-1]
-            G.add_edge(module_parent, module_name, color="black", penwidth='3')
+            if CONFIG["edge"]["hierarchy"] is not None:
+                G.add_edge(module_parent, module_name, **CONFIG["edge"]["hierarchy"])
 
         # Get imports
         imported_modules: list[str] = get_imports(source_code)
@@ -69,18 +119,19 @@ def build_graph(python_files: list[str], root: str) -> nx.DiGraph:
             if imported_module in module_names:
                 # Add edge for import, with label and color
                 if "__init__" in python_file:
-                    G.add_edge(module_name, imported_module, color="blue", penwidth='1', style="dashed")
+                    if CONFIG["edge"]["inits"] is not None:
+                        G.add_edge(module_name, imported_module, **CONFIG["edge"]["inits"])
                 else:
-                    G.add_edge(module_name, imported_module, color="red", penwidth='2')
+                    if CONFIG["edge"]["uses"] is not None:
+                        G.add_edge(module_name, imported_module, **CONFIG["edge"]["uses"])
                 
     return G
 
 
 def write_dot(G: nx.DiGraph, output_filename: str) -> None:
     """Write graph to a DOT file"""
-    P = to_pydot(G)
-    # Set direction
-    P.set('rankdir', 'TB')  
+    P: pydot.Dot = to_pydot(G)
+    P.obj_dict["attributes"].update(CONFIG["dot_attrs"])
     P.write_raw(output_filename)
 
 
@@ -94,21 +145,78 @@ def main(
         root: str, 
         output: str, 
         output_fmt: Literal["svg", "png"] = "svg",
-        
+        config_file: str | None = None,
+        print_cfg: bool = False,
+        verbose: bool = False,
+        **kwargs,
     ) -> None:
-    """Main function to generate a DOT file representing module dependencies"""
+    """Main function to generate a DOT file representing module dependencies
+    
+    # Positional/keyword arguments
+    - `root: str`
+        root directory to search for Python files
+    - `output: str`
+        output filename (without extension)
+    - `output_fmt: Literal["svg", "png"]`
+        output format for running `dot`
+
+    # Keyword-only arguments
+    - `config_file: str | None = None`
+        path to a JSON file containing configuration
+    - `print_cfg: bool = False`
+        whether to print the configuration after loading it -- if this is set, the program will exit after printing the config
+    - `verbose: bool = False`
+    - `h` or `help`
+        print this help message
+    
+    # Configuration options
+    either specify these in a json file, or separate levels with a dot. set to `None` to disable.
+    - `node.dir` kwargs for directory nodes
+    - `node.file` kwargs for file nodes
+    - `edge.heirarchy` kwargs for hierarchy edges (i.e. file A is part of module B)
+    - `edge.uses` kwargs for uses edges (i.e. file A imports module B for using it)
+    - `edge.inits` kwargs for init edges (i.e. __init__.py file imports something from downstream of itself)
+    """
+
+    if config_file is not None:
+        with open(config_file, "r", encoding="utf-8") as f:
+            update_with_nested_dict(CONFIG, json.load(f))
+
+    if len(kwargs) > 0:
+        update_with_nested_dict(
+            CONFIG, 
+            kwargs_to_nested_dict(kwargs, transform_key=lambda x: x.lstrip("-"), sep="."),
+        )
+
+    _process_config()
+
+    if "h" in CONFIG or "help" in CONFIG:
+        print(main.__doc__)
+        print("# current config:")
+        print(json.dumps(CONFIG, indent=2))
+        exit()
+    
+    if print_cfg:
+        print(json.dumps(CONFIG, indent=2))
+        exit()
+
     print("# getting python files...")
     python_files: list[str] = get_python_files(root)
     print(f"\t found {len(python_files)} python files")
     print("# building graph...")
-    G = build_graph(python_files, root)
+    G: nx.MultiDiGraph = build_graph(python_files, root)
     print(f"\t built graph with {len(G.nodes)} nodes and {len(G.edges)} edges")
     
     output_file_dot: str = f"{output}.dot"
     print(f"# writing dot file: {output_file_dot}")
     write_dot(G, f"{output_file_dot}")
-    print(f"# writing {output_fmt} file: {output}.{output_fmt}")
-    os.system(f"dot -T{output_fmt} {output_file_dot} -o {output}.{output_fmt}")
+
+    print(f"# running dot...")
+    print(f"\tcommand:")
+    cmd: str = f"dot -T{output_fmt} {output_file_dot} -o {output}.{output_fmt} {'-v' if verbose else ''}"
+    print(f"\t$ {cmd}")
+    os.system(cmd)
+
     print("# done!")
 
 
