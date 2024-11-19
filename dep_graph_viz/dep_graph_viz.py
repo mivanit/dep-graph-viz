f7c952d0ee4fd22afd83ac67230ea1de871666a5
mivanit <mivanits@umich.edu>
2024-11-19 14:19:46 -0700

    refactor out some file and import funcs

--- dep_graph_viz/dep_graph_viz.py
+++ dep_graph_viz/dep_graph_viz.py
@@ -1,10 +1,7 @@
-import ast
-import glob
 import json
 import os
 import subprocess
 from dataclasses import dataclass
-from pathlib import Path
 from typing import Any, Literal
 
 import networkx as nx
@@ -14,6 +11,11 @@ from networkx.drawing.nx_pydot import to_pydot
 
 from dep_graph_viz.config import CONFIG, _process_config
 from dep_graph_viz.util.paths import normalize_path, path_to_module
+from dep_graph_viz.util.util import (
+	get_imports,
+	get_python_files,
+	get_relevant_directories,
+)
 
 ORIG_DIR: str = os.getcwd()
 # *absolute* path of the root directory
@@ -41,77 +43,6 @@ def add_node(G: nx.MultiDiGraph, node: "Node") -> None:
 			G.nodes[node]["URL"] = f'"{node.url}"'
 	else:
 		raise ValueError(f"node {node.path} already exists in the graph!")
-
-
-def get_imports(source_code: str) -> list[str]:
-	"Get all the imports from a source code string"
-	tree: ast.Module = ast.parse(source_code)
-	imports: list[str] = []
-	for node in ast.walk(tree):
-		# Check if node is an import statement
-		if isinstance(node, ast.Import):
-			for alias in node.names:
-				if alias.name is None:
-					raise ValueError(
-						f"node.names[alias].name is None: {node = } {alias = }"
-					)
-				imports.append(alias.name)
-		# Check if node is a from ... import ... statement
-		elif isinstance(node, ast.ImportFrom):
-			if node.module is None:
-				raise ValueError(f"module name is None: {node = }")
-			imports.append(node.module)
-
-	return imports
-
-
-def get_python_files(root: str = ".") -> list[str]:
-	"Get all Python files in a directory and its subdirectories"
-	if not os.path.exists(root):
-		raise FileNotFoundError(f"root directory not found: {root}")
-
-	glob_pattern: str = Path(root).as_posix().rstrip("/") + "/**/*.py"
-	files: list[str] = glob.glob(glob_pattern, recursive=True)
-	files = [
-		Path(os.path.relpath(file, os.path.abspath(root))).as_posix() for file in files
-	]
-	return files
-
-
-def get_relevant_directories(root: str = ".") -> set[str]:
-	"from a root, get a set of all directories with python files in them"
-	if not os.path.exists(root):
-		raise FileNotFoundError(f"root directory not found: {root}")
-
-	# get all directories with python files
-	directories_with_py_files: set[str] = {
-		os.path.dirname(file) for file in get_python_files(root)
-	}
-	directories_with_py_files = {
-		x if x != "" else "."  # map empty string to root
-		for x in directories_with_py_files
-	}
-	# allocate output
-	all_directories: set[str] = set(directories_with_py_files)
-	all_directories.add(".")
-
-	# get every directory between root and root dir
-	# since some directories might have no python files, but contain dirs with python files
-	for directory in directories_with_py_files:
-		while directory and os.path.abspath(directory) != os.path.abspath(root):
-			parent_directory = os.path.dirname(directory)
-			if parent_directory and parent_directory != directory:
-				all_directories.add(parent_directory)
-			directory = parent_directory
-
-	if "" in all_directories:
-		raise ValueError(f"empty string found in {all_directories = }")
-	all_directories = set(map(normalize_path, all_directories))
-
-	return all_directories
-
-
-# =================================================================
 
 
 def classify_node(path: str, root: str = ".") -> NodeType:
---
+++ dep_graph_viz/util/util.py
@@ -0,0 +1,75 @@
+import ast
+import glob
+import os
+from pathlib import Path
+
+
+from dep_graph_viz.util.paths import normalize_path
+
+
+def get_imports(source_code: str) -> list[str]:
+	"Get all the imports from a source code string"
+	tree: ast.Module = ast.parse(source_code)
+	imports: list[str] = []
+	for node in ast.walk(tree):
+		# Check if node is an import statement
+		if isinstance(node, ast.Import):
+			for alias in node.names:
+				if alias.name is None:
+					raise ValueError(
+						f"node.names[alias].name is None: {node = } {alias = }"
+					)
+				imports.append(alias.name)
+		# Check if node is a from ... import ... statement
+		elif isinstance(node, ast.ImportFrom):
+			if node.module is None:
+				raise ValueError(f"module name is None: {node = }")
+			imports.append(node.module)
+
+	return imports
+
+
+def get_python_files(root: str = ".") -> list[str]:
+	"Get all Python files in a directory and its subdirectories"
+	if not os.path.exists(root):
+		raise FileNotFoundError(f"root directory not found: {root}")
+
+	glob_pattern: str = Path(root).as_posix().rstrip("/") + "/**/*.py"
+	files: list[str] = glob.glob(glob_pattern, recursive=True)
+	files = [
+		Path(os.path.relpath(file, os.path.abspath(root))).as_posix() for file in files
+	]
+	return files
+
+
+def get_relevant_directories(root: str = ".") -> set[str]:
+	"from a root, get a set of all directories with python files in them"
+	if not os.path.exists(root):
+		raise FileNotFoundError(f"root directory not found: {root}")
+
+	# get all directories with python files
+	directories_with_py_files: set[str] = {
+		os.path.dirname(file) for file in get_python_files(root)
+	}
+	directories_with_py_files = {
+		x if x != "" else "."  # map empty string to root
+		for x in directories_with_py_files
+	}
+	# allocate output
+	all_directories: set[str] = set(directories_with_py_files)
+	all_directories.add(".")
+
+	# get every directory between root and root dir
+	# since some directories might have no python files, but contain dirs with python files
+	for directory in directories_with_py_files:
+		while directory and os.path.abspath(directory) != os.path.abspath(root):
+			parent_directory = os.path.dirname(directory)
+			if parent_directory and parent_directory != directory:
+				all_directories.add(parent_directory)
+			directory = parent_directory
+
+	if "" in all_directories:
+		raise ValueError(f"empty string found in {all_directories = }")
+	all_directories = set(map(normalize_path, all_directories))
+
+	return all_directories
