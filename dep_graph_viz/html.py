2be9549f6ed9e81292bd48977129674c3ae8bdd1
mivanit <mivanits@umich.edu>
2024-11-19 09:54:25 -0700

    split out template, use importlib.resources

--- dep_graph_viz/html.py
+++ dep_graph_viz/html.py
@@ -1,53 +1,8 @@
-HTML_TEMPLATE: str = r"""
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Static Graphviz SVG</title>
-    <script src="https://d3js.org/d3.v5.min.js"></script>
-    <script src="https://unpkg.com/@hpcc-js/wasm@0.3.13/dist/index.min.js"></script>
-    <script src="https://unpkg.com/d3-graphviz@3.1.0/build/d3-graphviz.min.js"></script>
-    <style>
-        .node:hover,
-        .edge.highlighted {
-            stroke: red;
-            stroke-width: 2px;
-        }
-        .edge.highlighted[marker-end] {
-            marker-end: url(#arrowhead-red);
-        }
-    </style>
-</head>
-<body>
-    <div id="graph" style="width: 100%; height: 100vh;"></div>
-    <script>
-        const dot = `$$DOT_CONTENT$$`;
+import importlib.resources
 
-        d3.select("#graph").graphviz()
-            .renderDot(dot)
-            .on("end", function() {
-                const graphviz = this;
+import dep_graph_viz
 
-                d3.selectAll(".node")
-                    .on("mouseover", function(d) {
-                        const nodeId = d3.select(this).attr("id");
-                        console.log("Hovered Node ID:", nodeId);
-
-                        const connectedEdges = graphviz.inEdges(nodeId).concat(graphviz.outEdges(nodeId));
-                        console.log("Connected Edges:", connectedEdges);
-
-                        d3.selectAll(".edge")
-                            .classed("highlighted", function(d) {
-                                return connectedEdges.includes(d3.select(this).node());
-                            });
-                    })
-                    .on("mouseout", function(d) {
-                        d3.selectAll(".edge").classed("highlighted", false);
-                    });
-            });
-    </script>
-</body>
-</html>
-"""
+HTML_TEMPLATE: str = importlib.resources.files(dep_graph_viz).joinpath("template.html").read_text()
 
 
 def generate_html(dot_file_path: str, output_html_path: str):
