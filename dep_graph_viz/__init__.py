686b1ba23d3e981ab10fb56636ce20635aa8a845
mivanit <mivanits@umich.edu>
2024-11-19 21:59:10 -0700

    add __all__ to __init__.py files

--- dep_graph_viz/__init__.py
+++ dep_graph_viz/__init__.py
@@ -1,5 +1,16 @@
+"""
+.. include:: ../README.md
+"""
+
 from dep_graph_viz.dep_graph_viz import main
 
 __all__ = [
+	# actually imported function
 	"main",
+	# modules
+	"__main__",
+	"dep_graph_viz",
+	"config",
+	"html",
+	"util",
 ]
--- dep_graph_viz/util/__init__.py
+++ dep_graph_viz/util/__init__.py
@@ -3,4 +3,7 @@ from dep_graph_viz.util.paths import normalize_path, path_to_module
 __all__ = [
 	"normalize_path",
 	"path_to_module",
+	# submodules
+	"paths",
+	"util",
 ]
